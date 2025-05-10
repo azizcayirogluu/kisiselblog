a9d859178a8b9b83e9419d2c3906818dc6cd461b
Aziz Ç. <azizcayirogluu@gmail.com>
2025-05-10 14:01:11 +0300

    Add files via upload

---
+++ aziz.js
@@ -0,0 +1,12 @@
+<div style="text-align:center; margin-top: 80px;">
+  <lottie-player
+    src="https://assets7.lottiefiles.com/packages/lf20_usmfx6bp.json"
+    background="transparent"
+    speed="1"
+    style="width: 250px; height: 250px;"
+    loop
+    autoplay
+  ></lottie-player>
+  <h2>Bu alan yakında dolacak!</h2>
+  <p>Şu an için içerik bulunmamaktadır. Takipte kalın 🛠️</p>
+</div>;
